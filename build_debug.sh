4fa9a645c3b8a9401483f36824fe6d4deae4b135
luke pc <lazarluke02@gmail.com>
2026-01-04 02:13:24 +1100

    Intellisense working in vscode and nvim

--- build_debug.sh
+++ build_debug.sh
@@ -1,3 +1,3 @@
 cmake -S . -B debug_build -DCMAKE_BUILD_TYPE=Debug
 cmake --build debug_build
-
+ln -s debug_build/compile_commands.json .
--- build_release.sh
+++ build_release.sh
@@ -1,2 +1,3 @@
 cmake -S . -B release_build -DCMAKE_BUILD_TYPE=Release
 cmake --build release_build
+ln -s release_build/compile_commands.json .
